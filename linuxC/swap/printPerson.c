fce2c6d0a08cca2047b8478dca51f3b3dfac4eb7
Felix Duan <felixduan@iuni.com>
2014-07-25 09:39:26 +0800

    write funs do jobs

--- linuxC/swap/printPerson.c
+++ linuxC/swap/printPerson.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "person.h"
 
-void * initPerson(person* p, char* n, char* g, int a, char* birth) {
+void* initPerson(person* p, char* n, char* g, int a, char* birth) {
     p->name = n;
     p->gender = g;
     p->age = a;
     p->birthday = birth;
+    return;
 }
 
+
+void printPerson(person* p) {
+    printf("%s\t%s\t%d\t%s\n", p->name, p->gender, p->age, p->birthday);
+    return;
+}
 
 void main() {
     printf("hello print person\n");
 
-    char* name = "kyle";
-    char* gender = "M";
-    int age = 30;
-    char* birthday = "1092-5-21";
-    printf("%s\n",name);
     person *p1 = malloc(sizeof(person));
-    initPerson(p1, name, gender, age, birthday);
-    //p1->name = s;
-    //*p1->gender = "M";
-    //p1->age = 30;
-    //*p1->birthday = "1978-2-41";
-    printf("%s\n", p1->name);
-    printf("%s\n", p1->gender);
-    printf("%d\n", p1->age);
-    printf("%s\n", p1->birthday);
+    person *p2 = malloc(sizeof(person));
+
+    initPerson(p1, "Kyle", "M", 30, "1042-66-1");
+    initPerson(p2, "Padma", "M", 21, "1235-6-1");
+
+    printPerson(p1);
+    printPerson(p2);
     return;
 }
